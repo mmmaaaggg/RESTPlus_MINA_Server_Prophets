5df0973eb737c62de1328e733928b3dc8b7dc379
mmmaaaggg <mmmaaaggg@163.com>
2018-07-12 15:59:37 +0800

    refine run方法

--- run.py
+++ run.py
@@ -9,6 +9,15 @@
 """
 
 if __name__ == '__main__':
+    import logging
+    from app.config import config
     from app.app import app
+    logger = logging.getLogger()
+    if config.APP_ENABLE_SSL:
+        logger.info('ssl path: %s', config.HTTPS_SSL_PEM_FILE_PATH)
 
-    app.run(debug=True)
+    app.run(
+        host='0.0.0.0', port=config.APP_PORT, debug=True,
+        ssl_context='adhoc',
+        # ssl_context=(config.HTTPS_SSL_PEM_FILE_PATH, config.HTTPS_SSL_KEY_FILE_PATH) if config.APP_ENABLE_SSL else None
+    )
